ed343fcf97719c78ea30110e2ebe99306b3ff25b
lasc4 <lmedi2200@inwoodearlycollege.org>
2017-08-08 16:00:50 -0400

    leonard

---
+++ leonard-personalsite/sketch.js
@@ -0,0 +1,8 @@
+
+$(body).ready(function(){
+    $("button").click(function(){
+        $("#div1").fadeToggle();
+        $("#div2").fadeToggle("slow");
+        $("#div3").fadeToggle(3000);
+    })
+});
